d75fc615a25cfaa44f7b26eed6c36a548461e277
holgermueller <9jholger@gmail.com>
2022-10-18 21:14:38 -0500

    some styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <main>
-    <img :src="imageData.url" alt="Today's picture" />
-    {{ imageData.copyright }}
+  <main
+    :style="{
+      background: 'url(' + imageData.photos[0].img_src + ') ',
+    }"
+  >
+    <h1>Today's Image</h1>
+
+    <h2>Courtesy of: {{ imageData.photos[0].rover.name }}</h2>
   </main>
 </template>
 
@@ -11,7 +16,7 @@ export default {
   components: {},
 
   created() {
-    this.$store.dispatch("getImage").then(() => console.log("weather fetched"));
+    this.$store.dispatch("getImage").then(() => console.log("image fetched"));
   },
 
   computed: {
@@ -24,12 +29,32 @@ export default {
 </script>
 
 <style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  height: 100vh;
+}
+h1,
+h2 {
+  margin: 0 auto;
+  padding: 8px;
+  background-color: hsla(0, 83%, 50%);
+  color: #080808;
+  width: max-content;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -15,7 +15,10 @@ const store = createStore({
     async getImage({ commit }) {
       const API_KEY = process.env.VUE_APP_MARS_WEATHER_KEY;
 
-      await fetch("https://api.nasa.gov/planetary/apod?api_key=" + API_KEY)
+      await fetch(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=" +
+          API_KEY
+      )
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
