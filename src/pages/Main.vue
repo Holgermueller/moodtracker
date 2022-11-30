375789a90f2eb627ae86fbda550f00552869ec95
holgermueller <9jholger@gmail.com>
2022-11-30 16:04:03 -0600

    got dummy data on dom

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,3 +1,81 @@
 <template>
-  <div id="main"><h1>Main</h1></div>
+  <div id="main">
+    <section class="moods-display">
+      <p>Hello, {{ userProfile.username }}</p>
+
+      <p>Instances tracked: {{ instancesTracked }}</p>
+
+      <div class="graphs">
+        <div
+          class="mood"
+          v-for="mood in userProfile.userMoods"
+          :key="mood.mood"
+        >
+          <div
+            class="single-mood"
+            :class="mood.mood"
+            :style="`width: ${
+              Math.floor((100 / instancesTracked) * mood.timesFelt) || 0
+            }%;
+          background: ${mood.color}; border-left: 4px solid ${mood.borderColor};
+          `"
+          ></div>
+          <span> {{ mood.mood }} | Times felt:{{ mood.timesFelt }} </span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "moodsDisplay",
+
+  data: () => ({}),
+
+  computed: {
+    userProfile() {
+      return this.$store.getters.dummyData[0];
+    },
+
+    instancesTracked() {
+      return this.$store.getters.dummyData[0].userMoods.reduce(
+        (a, { timesFelt }) => a + timesFelt,
+        0
+      );
+    },
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+.moods-display {
+  width: 45%;
+  margin: 8px auto;
+}
+
+.graphs {
+  width: 75%;
+  margin: 0 auto;
+}
+
+.mood {
+  background: #fafafa;
+  margin: 10px auto;
+  position: relative;
+}
+
+.single-mood {
+  height: 100%;
+  position: absolute;
+  box-sizing: border-box;
+}
+
+span {
+  text-align: center;
+  display: inline-block;
+  padding: 10px 20px;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -3,7 +3,67 @@ import { createStore } from "vuex";
 const store = createStore({
   state: {
     data: {},
-    dummyData: [],
+    dummyData: [
+      {
+        username: "Namey Name",
+        userMoods: [
+          {
+            mood: "Bored",
+            timesFelt: 1,
+            borderColor: "green",
+            color: "rgba(69, 196, 150, 0.2)",
+          },
+          {
+            mood: "Happy",
+            timesFelt: 2,
+            borderColor: "#ffd700",
+            color: "rgba(255, 215, 0, 0.2)",
+          },
+          {
+            mood: "Sad",
+            timesFelt: 3,
+            borderColor: "blue",
+            color: "rgba(0, 0, 255, 0.2)",
+          },
+          {
+            mood: "Tired",
+            timesFelt: 4,
+            borderColor: "#9932cc",
+            color: "rgba(153, 50, 204, 0.2)",
+          },
+          {
+            mood: "Anxious",
+            timesFelt: 5,
+            borderColor: "#ffa500",
+            color: "rgba(255, 165, 0, 0.2)",
+          },
+          {
+            mood: "Angry",
+            timesFelt: 6,
+            borderColor: "red",
+            color: "rgba(217, 27, 66, 0.2)",
+          },
+          {
+            mood: "Uncomfortable",
+            timesFelt: 7,
+            borderColor: "#40E0D0",
+            color: "rgba(64, 224, 208, 0.2)",
+          },
+          {
+            mood: "Calm",
+            timesFelt: 8,
+            borderColor: "#00005f",
+            color: "rgba(0, 0, 95, 0.2)",
+          },
+          {
+            mood: "Weird",
+            timesFelt: 9,
+            borderColor: "#ff00ff",
+            color: "rgba(255, 0, 255, 0.2)",
+          },
+        ],
+      },
+    ],
   },
 
   mutations: {
