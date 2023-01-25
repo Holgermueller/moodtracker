dc5dc48413de41d345fa071bfaac37d50ca2d2ae
holgermueller <9jholger@gmail.com>
2023-01-25 15:28:37 -0600

    added click events to buttons

--- src/components/MoodButtons.vue
+++ src/components/MoodButtons.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="moodButtons">
-    <p>How are you feeling today?</p>
-    <div v-for="mood in moods" :key="mood.mood" class="buttons-array">
-      <button
-        :style="`
+    <div class="mood-range-card">
+      <p>How are you feeling today?</p>
+      <div v-for="mood in moods" :key="mood.mood" class="buttons-array">
+        <button
+          :id="mood.mood"
+          class="mood-button"
+          :style="`
       border: 2px solid ${mood.borderColor};
       color: ${mood.borderColor};
       `"
-      >
-        {{ mood.mood }}
-      </button>
+          @click.prevent="incrementMood"
+        >
+          {{ mood.mood }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,20 +31,29 @@ export default {
 
   data: () => ({}),
 
-  methods: {},
+  methods: {
+    incrementMood(e) {
+      console.log(e.currentTarget.id);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.button-array {
+p {
+  width: fit-content;
+  margin: 4px auto;
+  font-size: large;
+  font-weight: bold;
+}
+.buttons-array {
   display: inline;
   justify-content: center;
-  padding: 8px;
 }
 
-button {
-  margin: auto;
-  border-radius: 5px;
-  width: 15%;
+mood-button {
+  margin: 4px auto;
+  border-radius: 7px;
+  width: 100%;
 }
 </style>
